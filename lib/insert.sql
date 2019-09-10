befc0f9728a8571a5a1bf70cd75b9e560994ab2e
Robinson Regalado <rregalado17@gmail.com>
2019-09-10 14:43:29 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series VALUES (1, "A New Hope", 1, 1);
+INSERT INTO series VALUES (2, "The Force Awakens", 2, 2);
+
+INSERT INTO books VALUES (1, "John", 25);
+INSERT INTO books VALUES (2, "John", 25);
+INSERT INTO books VALUES (3, "John", 25);
+INSERT INTO books VALUES (4, "John", 25);
+INSERT INTO books VALUES (5, "John", 25);
+INSERT INTO books VALUES (6, "John", 25);
+
+INSERT INTO characters VALUES (1, "John", 25);
+INSERT INTO characters VALUES (2, "John", 25);
+INSERT INTO characters VALUES (3, "John", 25);
+INSERT INTO characters VALUES (4, "John", 25);
+INSERT INTO characters VALUES (5, "John", 25);
+INSERT INTO characters VALUES (6, "John", 25);
+INSERT INTO characters VALUES (7, "John", 25);
+INSERT INTO characters VALUES (8, "John", 25);
+
+INSERT INTO subgenres VALUES (1, "John", 25);
+INSERT INTO subgenres VALUES (2, "John", 25);
+
+INSERT INTO authors VALUES (1, "John", 25);
+INSERT INTO authors VALUES (2, "John", 25);
+
+INSERT INTO character_books VALUES (1, "John", 25);
+INSERT INTO character_books VALUES (2, "John", 25);
+INSERT INTO character_books VALUES (3, "John", 25);
+INSERT INTO character_books VALUES (4, "John", 25);
+INSERT INTO character_books VALUES (5, "John", 25);
+INSERT INTO character_books VALUES (6, "John", 25);
+INSERT INTO character_books VALUES (7, "John", 25);
+INSERT INTO character_books VALUES (8, "John", 25);
+INSERT INTO character_books VALUES (9, "John", 25);
+INSERT INTO character_books VALUES (10, "John", 25);
+INSERT INTO character_books VALUES (11, "John", 25);
+INSERT INTO character_books VALUES (12, "John", 25);
+INSERT INTO character_books VALUES (13, "John", 25);
+INSERT INTO character_books VALUES (14, "John", 25);
+INSERT INTO character_books VALUES (15, "John", 25);
+INSERT INTO character_books VALUES (16, "John", 25);
